548f21aa3bf402c48bceeab35fb83b2b1445c848
Minghui Zhu <mz223@duke.edu>
2024-07-22 12:29:44 -0400

    Feat: Update one Feature Project

--- src/components/featured-projects/featured-projects.js
+++ src/components/featured-projects/featured-projects.js
@@ -7,19 +7,20 @@ const FeaturedProjects = () => (
     <div className="section__content">
       <div className="project blistabloc">
         <figure className="project__pic">
-          <a href="https://passkeynotes.com/" rel="noreferrer" target="_blank">
-            <img src="./images/featured/passkeynotes/frame.png" alt=""/>
+          <a href="https://github.com/zhuminghui17/ECE564-AssetMgmt/" rel="noreferrer" target="_blank">
+            <img src="./images/featured/asset-mgmt/frame.png" alt=""/>
           </a>
         </figure>
         <figcaption className="project__caption">
           <div className="project__name">
-            <a href="https://passkeynotes.com/" rel="noreferrer" target="_blank">Passkey Notes</a>
+            <a href="https://github.com/zhuminghui17/ECE564-AssetMgmt/" rel="noreferrer" target="_blank">Duke Asset Management iOS App</a>
           </div>
-          <p>A Secure Notes taking app, which syncs data across all your devices and stores all the data with AES-256 encryption</p>
+          <p>The Duke Asset Management iOS Mobile App serves as a mobile gateway to MediaBeacon, Duke's extensive digital asset management system. This application enables users to seamlessly view, download, and upload files while ensuring that all modifications are synchronized with the MediaBeacon web server. Designed for convenience and efficiency, this mobile interface is a significant step forward in digital asset management.
+</p>
         </figcaption>
       </div>
 
-      <div className="project screentime">
+      {/* <div className="project screentime">
         <figure className="project__pic phones">
           <a href="https://play.google.com/store/apps/details?id=com.talkingyak.app" rel="noreferrer" target="_blank">
             <div className="phone">
@@ -39,7 +40,7 @@ const FeaturedProjects = () => (
           </div>
           <p>Talking Yak is an English learning app specifically created for Hindi and Tamil speakers.</p>
         </figcaption>
-      </div>
+      </div> */}
     </div>
   </section>
 )
