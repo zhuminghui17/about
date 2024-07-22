602dedcd4cd7b38749038792fee403d56c9d4c6c
Minghui Zhu <mz223@duke.edu>
2024-07-22 14:21:27 -0400

    Feat: Add logo test

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,9 +2,10 @@ import * as React from "react"
 
 const seoData = {
   "title": "Matt Zhu",
-  "description": "Matt Zhu is a an expert in top-tier mobile/web app architecture & robust backend servers",
+  "description": "Matt Zhu is an expert in top-tier mobile/web app architecture & robust backend servers",
   "author": "Matt Zhu",
   "siteUrl": "https://mhzmat.netlify.app/",
+  "logo": "/logo4.png", 
 }
 
 const Seo = () => (
@@ -14,10 +15,13 @@ const Seo = () => (
     <meta property="og:title" content={seoData.title} />
     <meta property="og:description" content={seoData.description} />
     <meta property="og:type" content="website" />
-    <meta name="twitter:card" content="summary" />
+    <meta property="og:image" content={`${seoData.siteUrl}${seoData.logo}`} />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content={seoData.author} />
     <meta name="twitter:title" content={seoData.title} />
     <meta name="twitter:description" content={seoData.description} />
+    <meta name="twitter:image" content={`${seoData.siteUrl}${seoData.logo}`} />
+    <link rel="icon" href={seoData.logo} />
   </>
 )
 
